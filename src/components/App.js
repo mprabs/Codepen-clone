15d8f4fae6da437741139f651df02a56fc98ab43
mprabs <acharyaprabin03@gmail.com>
2020-09-20 11:36:46 +0545

    design and code wrapping

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Editor from './editor'
+
+function App() {
+const [html, setHtml] = useState('')
+const [css, setCss] = useState('')
+const [javaScript, setJavaScript] = useState('')
+  return (
+    <>
+    <div className="pane top-pane">
+      <Editor
+        language="xml"
+        displayName="HTML"
+        value={html}
+        onChange={setHtml}
+       />
+      <Editor
+        language="css"
+        displayName="CSS"
+        value={css}
+        onChange={setCss}
+       />
+       <Editor
+        language="javascript"
+        displayName="JS"
+        value={javaScript}
+        onChange={setJavaScript}
+       />
+    </div>
+    <div className="pane">
+      <iframe 
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/editor.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+
+function editor(props) {
+    const { language, displayName,
+        value,
+        onChange
+     } = props
+
+     function handleChange(editor, data, value) {
+         onChange(value)
+     }
+
+    return (
+        <div className="editor-container">
+            <div className="editor-title">
+                {displayName}
+                <button>O/C</button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper"
+                options={{
+                    lineNumbers: true,
+                    lineWrapping:true,
+                    lint:true,
+                    mode: language,
+                    theme: 'material'
+                }}
+             />
+        </div>
+    )
+}
+
+export default editor
